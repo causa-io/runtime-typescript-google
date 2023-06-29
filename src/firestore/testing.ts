eb5b8b5d40edd5f2b36ef0785652b4c2bcb1b05d
Flo <florent.vouin@gmail.com>
2023-06-29 17:16:53 +0200

    ✨ Implement module-related Firestore testing utilities

--- src/firestore/testing.ts
+++ src/firestore/testing.ts
@@ -1,7 +1,10 @@
+import { INestApplicationContext } from '@nestjs/common';
+import { TestingModuleBuilder } from '@nestjs/testing';
 import { CollectionReference, Firestore } from 'firebase-admin/firestore';
 import * as uuid from 'uuid';
 import { getFirestoreCollectionNameForType } from './collection-name.decorator.js';
 import { makeFirestoreDataConverter } from './converter.js';
+import { getFirestoreCollectionInjectionName } from './inject-collection.decorator.js';
 
 /**
  * Creates a new collection prefixed with a random ID.
@@ -38,4 +41,44 @@ export async function clearFirestoreCollection(
   documents.forEach((document) => batch.delete(document));
 
   await batch.commit();
+}
+
+/**
+ * Overrides the providers for Firestore collections with temporary collections.
+ *
+ * @param builder The {@link TestingModuleBuilder} used to override providers.
+ * @param documentTypes The types of documents corresponding to Firestore collections, for which collections should be
+ *   overridden.
+ * @returns The {@link TestingModuleBuilder} with the overridden providers.
+ */
+export function overrideFirestoreCollections(
+  builder: TestingModuleBuilder,
+  ...documentTypes: { new (): any }[]
+): TestingModuleBuilder {
+  documentTypes.forEach((documentType) => {
+    builder = builder
+      .overrideProvider(getFirestoreCollectionInjectionName(documentType))
+      .useFactory({
+        factory: (firestore: Firestore) =>
+          createFirestoreTemporaryCollection(firestore, documentType),
+        inject: [Firestore],
+      });
+  });
+
+  return builder;
+}
+
+/**
+ * Returns the {@link CollectionReference} of the Firestore collection corresponding to the given class.
+ * Retrieving it from the module or application ensures the "mocked" collection is used.
+ *
+ * @param context The NestJS module or application from which the Firestore collection should be retrieved.
+ * @param documentType The type of document stored in the collection.
+ * @returns The {@link CollectionReference} of the Firestore collection.
+ */
+export function getFirestoreCollectionFromModule<T>(
+  context: INestApplicationContext,
+  documentType: { new (): T },
+): CollectionReference<T> {
+  return context.get(getFirestoreCollectionInjectionName(documentType));
 }
