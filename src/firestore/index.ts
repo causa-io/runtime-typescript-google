2ff7028b1069be41765fc242340941827f8f65dd
Flo <florent.vouin@gmail.com>
2023-06-29 15:27:13 +0200

    ✨ Implement the FirestoreCollectionsModule

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -1,5 +1,7 @@
 export { FirestoreCollectionName } from './collection-name.decorator.js';
+export { FirestoreCollectionsModule } from './collections.module.js';
 export {
   convertFirestoreTimestampsToDates,
   makeFirestoreDataConverter,
 } from './converter.js';
+export { InjectFirestoreCollection } from './inject-collection.decorator.js';
