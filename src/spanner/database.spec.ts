940e6fc991dcb83104fbb53e0803d3b04eae8120
Flo <florent.vouin@gmail.com>
2023-06-30 12:08:21 +0200

    ✨ Define the default Spanner configuration and database

---
+++ src/spanner/database.spec.ts
@@ -0,0 +1,49 @@
+import { getLoggedInfos, spyOnLogger } from '@causa/runtime/logging/testing';
+import { Database } from '@google-cloud/spanner';
+import { getDefaultSpannerDatabaseForCloudFunction } from './database.js';
+import { createDatabase } from './testing.js';
+
+describe('database', () => {
+  let database: Database;
+  let previousEnv: NodeJS.ProcessEnv;
+
+  beforeAll(async () => {
+    database = await createDatabase();
+    previousEnv = process.env;
+    process.env.SPANNER_DATABASE = database.formattedName_.split('/').at(-1);
+
+    spyOnLogger();
+  });
+
+  afterAll(async () => {
+    await database.getMetadata(); // Avoids errors when deleting the database too quickly.
+    await database.delete();
+    process.env = previousEnv;
+  });
+
+  it('should return the singleton default database', () => {
+    const db1 = getDefaultSpannerDatabaseForCloudFunction();
+    const db2 = getDefaultSpannerDatabaseForCloudFunction();
+
+    // The first two components are `projects/<projectId>`.
+    // However `db1` still uses a placeholder (`{{projectId}}`), while `database` has the actual (demo) project ID.
+    expect(db1.formattedName_.split('/').slice(2)).toEqual(
+      database.formattedName_.split('/').slice(2),
+    );
+    expect(db1).toBe(db2);
+  });
+
+  it('should catch emitted errors', async () => {
+    const db = getDefaultSpannerDatabaseForCloudFunction();
+
+    db.emit('error', new Error('💥'));
+
+    expect(getLoggedInfos()).toEqual([
+      expect.objectContaining({
+        message:
+          'Uncaught Spanner database error. This might be due to the background keep alive mechanism running in an idle Cloud Function.',
+        error: expect.stringContaining('💥'),
+      }),
+    ]);
+  });
+});
--- src/spanner/index.ts
+++ src/spanner/index.ts
@@ -1,3 +1,9 @@
 export { SpannerColumn } from './column.decorator.js';
+export {
+  SPANNER_SESSION_POOL_OPTIONS_FOR_CLOUD_FUNCTIONS,
+  SPANNER_SESSION_POOL_OPTIONS_FOR_SERVICE,
+  catchSpannerDatabaseErrors,
+  getDefaultSpannerDatabaseForCloudFunction,
+} from './database.js';
 export { SpannerTable } from './table.decorator.js';
 export * from './types.js';
