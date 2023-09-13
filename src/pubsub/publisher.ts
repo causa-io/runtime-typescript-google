ba6e59f72cad92592f3eb09f76211ff383df4801
Flo <florent.vouin@gmail.com>
2023-09-13 14:56:53 +0200

    ✨ Disable batching by default and allow per topic configuration

--- src/pubsub/publisher.ts
+++ src/pubsub/publisher.ts
@@ -18,6 +18,15 @@ import { getConfigurationKeyForTopic } from './configuration.js';
 import { PubSubTopicNotConfiguredError } from './errors.js';
 
 /**
+ * The default options to use when publishing messages.
+ * Batching is disabled as the most common use case is to publish a single message at a time, for which latency is more
+ * important than throughput.
+ */
+const DEFAULT_PUBLISH_OPTIONS: TopicPublishOptions = {
+  batching: { maxMessages: 1 },
+};
+
+/**
  * Options for the {@link PubSubPublisher}.
  */
 export type PubSubPublisherOptions = {
@@ -40,10 +49,17 @@ export type PubSubPublisherOptions = {
   configurationGetter?: (key: string) => string | undefined;
 
   /**
-   * The options to use when publishing messages.
+   * The default options to use when publishing messages.
    * This is used to instantiate the Pub/Sub {@link Topic}s.
    */
   publishOptions?: TopicPublishOptions;
+
+  /**
+   * The options to use when publishing messages.
+   * Keys are event topics, values are {@link TopicPublishOptions}.
+   * This inherits and overrides the {@link PubSubPublisherOptions.publishOptions} for a given topic.
+   */
+  topicPublishOptions?: Record<string, TopicPublishOptions>;
 
   /**
    * The logger to use.
@@ -89,6 +105,13 @@ export class PubSubPublisher implements EventPublisher, OnApplicationShutdown {
   readonly publishOptions: TopicPublishOptions | undefined;
 
   /**
+   * The options to use when publishing messages.
+   * Keys are event topics, values are {@link TopicPublishOptions}.
+   * This inherits and overrides the {@link PubSubPublisher.publishOptions} for a given topic.
+   */
+  readonly topicPublishOptions: Record<string, TopicPublishOptions>;
+
+  /**
    * Creates a new {@link PubSubPublisher}.
    *
    * @param options Options for the publisher.
@@ -98,7 +121,8 @@ export class PubSubPublisher implements EventPublisher, OnApplicationShutdown {
     this.serializer = options.serializer ?? new JsonObjectSerializer();
     this.getConfiguration =
       options.configurationGetter ?? ((key) => process.env[key]);
-    this.publishOptions = options.publishOptions;
+    this.publishOptions = options.publishOptions ?? DEFAULT_PUBLISH_OPTIONS;
+    this.topicPublishOptions = options.topicPublishOptions ?? {};
     this.logger = options.logger ?? getDefaultLogger();
   }
 
@@ -121,7 +145,11 @@ export class PubSubPublisher implements EventPublisher, OnApplicationShutdown {
       throw new PubSubTopicNotConfiguredError(topicName);
     }
 
-    const topic = this.pubSub.topic(topicId, this.publishOptions);
+    const options = {
+      ...this.publishOptions,
+      ...this.topicPublishOptions[topicName],
+    };
+    const topic = this.pubSub.topic(topicId, options);
 
     this.topicCache[topicName] = topic;
     return topic;
