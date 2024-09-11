a92b7d9a07c3a0eb2c874bd53916c5597ce17cf1
Flo <florent.vouin@gmail.com>
2024-09-11 17:10:07 +0200

    ✅ Refactor tests

--- src/tasks/scheduler.spec.ts
+++ src/tasks/scheduler.spec.ts
@@ -25,110 +25,118 @@ describe('CloudTasksScheduler', () => {
     scheduler = new CloudTasksScheduler(client);
   });
 
-  it('should create a regular task', async () => {
-    const scheduleDate = new Date();
-
-    const actualTask = await scheduler.schedule('MY_QUEUE', scheduleDate, {
-      body: '☑️',
-      httpMethod: HttpMethod.POST,
-      url: 'https://example.com',
-    });
-
-    expect(actualTask).toEqual(EXPECTED_TASK);
-    expect(client.createTask).toHaveBeenCalledWith(
-      {
-        parent: 'MY_QUEUE',
-        task: {
-          scheduleTime: {
-            seconds: Math.floor(scheduleDate.getTime() / 1000),
-            nanos: (scheduleDate.getTime() % 1000) * 1e6,
-          },
-          httpRequest: {
-            httpMethod: HttpMethod.POST,
-            url: 'https://example.com',
-            body: '☑️',
-          },
-        },
-      },
-      { retry: expect.any(Object) },
-    );
-    expect(client.auth.getCredentials).not.toHaveBeenCalled();
+  afterEach(async () => {
+    await client.close();
   });
 
-  it('should create a task with options', async () => {
-    const scheduleDate = new Date();
+  describe('schedule', () => {
+    it('should create a regular task', async () => {
+      const scheduleDate = new Date();
 
-    const actualTask = await scheduler.schedule(
-      'MY_QUEUE',
-      scheduleDate,
-      {
-        body: { doThis: '🤸' },
+      const actualTask = await scheduler.schedule('MY_QUEUE', scheduleDate, {
+        body: '☑️',
         httpMethod: HttpMethod.POST,
-        headers: {
-          'X-My-Header': '💡',
-        },
         url: 'https://example.com',
-        oidcToken: 'self',
-      },
-      {
-        retry: false,
-        taskName: 'MY_TASK',
-      },
-    );
+      });
 
-    expect(actualTask).toEqual(EXPECTED_TASK);
-    expect(client.createTask).toHaveBeenCalledWith(
-      {
-        parent: 'MY_QUEUE',
-        task: {
-          name: `MY_QUEUE/tasks/MY_TASK`,
-          scheduleTime: {
-            seconds: Math.floor(scheduleDate.getTime() / 1000),
-            nanos: (scheduleDate.getTime() % 1000) * 1e6,
-          },
-          httpRequest: {
-            httpMethod: HttpMethod.POST,
-            url: 'https://example.com',
-            body: expect.toSatisfy((body: Buffer) => {
-              expect(Buffer.from(body.toString(), 'base64').toString()).toEqual(
-                '{"doThis":"🤸"}',
-              );
-              return true;
-            }),
-            headers: {
-              'X-My-Header': '💡',
-              'Content-Type': 'application/json',
+      expect(actualTask).toEqual(EXPECTED_TASK);
+      expect(client.createTask).toHaveBeenCalledWith(
+        {
+          parent: 'MY_QUEUE',
+          task: {
+            scheduleTime: {
+              seconds: Math.floor(scheduleDate.getTime() / 1000),
+              nanos: (scheduleDate.getTime() % 1000) * 1e6,
             },
-            oidcToken: { serviceAccountEmail: EXPECTED_SERVICE_ACCOUNT_EMAIL },
+            httpRequest: {
+              httpMethod: HttpMethod.POST,
+              url: 'https://example.com',
+              body: '☑️',
+            },
           },
         },
-      },
-      {},
-    );
-    expect(client.auth.getCredentials).toHaveBeenCalledExactlyOnceWith();
-  });
-
-  it('should rethrow Cloud Tasks transient errors as retryable errors', async () => {
-    const deadlineExceeded = new Error('🕰️');
-    (deadlineExceeded as any).code = status.DEADLINE_EXCEEDED;
-    createTasksSpy.mockRejectedValueOnce(deadlineExceeded);
-
-    const actualPromise = scheduler.schedule('MY_QUEUE', new Date(), {});
-
-    await expect(actualPromise).rejects.toThrow(RetryableError);
-    await expect(actualPromise).rejects.toThrow('🕰️');
-  });
-
-  it('should rethrow credential transient errors as retryable errors', async () => {
-    const internal = new Error('🔧');
-    (internal as any).code = status.INTERNAL;
-    getCredentialsSpy.mockRejectedValueOnce(internal);
-
-    const actualPromise2 = scheduler.schedule('MY_QUEUE', new Date(), {
-      oidcToken: 'self',
+        { retry: expect.any(Object) },
+      );
+      expect(client.auth.getCredentials).not.toHaveBeenCalled();
     });
 
-    await expect(actualPromise2).rejects.toThrow(RetryableError);
-    await expect(actualPromise2).rejects.toThrow('🔧');
+    it('should create a task with options', async () => {
+      const scheduleDate = new Date();
+
+      const actualTask = await scheduler.schedule(
+        'MY_QUEUE',
+        scheduleDate,
+        {
+          body: { doThis: '🤸' },
+          httpMethod: HttpMethod.POST,
+          headers: {
+            'X-My-Header': '💡',
+          },
+          url: 'https://example.com',
+          oidcToken: 'self',
+        },
+        {
+          retry: false,
+          taskName: 'MY_TASK',
+        },
+      );
+
+      expect(actualTask).toEqual(EXPECTED_TASK);
+      expect(client.createTask).toHaveBeenCalledWith(
+        {
+          parent: 'MY_QUEUE',
+          task: {
+            name: `MY_QUEUE/tasks/MY_TASK`,
+            scheduleTime: {
+              seconds: Math.floor(scheduleDate.getTime() / 1000),
+              nanos: (scheduleDate.getTime() % 1000) * 1e6,
+            },
+            httpRequest: {
+              httpMethod: HttpMethod.POST,
+              url: 'https://example.com',
+              body: expect.toSatisfy((body: Buffer) => {
+                expect(
+                  Buffer.from(body.toString(), 'base64').toString(),
+                ).toEqual('{"doThis":"🤸"}');
+                return true;
+              }),
+              headers: {
+                'X-My-Header': '💡',
+                'Content-Type': 'application/json',
+              },
+              oidcToken: {
+                serviceAccountEmail: EXPECTED_SERVICE_ACCOUNT_EMAIL,
+              },
+            },
+          },
+        },
+        {},
+      );
+      expect(client.auth.getCredentials).toHaveBeenCalledExactlyOnceWith();
+    });
+
+    it('should rethrow Cloud Tasks transient errors as retryable errors', async () => {
+      const deadlineExceeded = new Error('🕰️');
+      (deadlineExceeded as any).code = status.DEADLINE_EXCEEDED;
+      createTasksSpy.mockRejectedValueOnce(deadlineExceeded);
+
+      const actualPromise = scheduler.schedule('MY_QUEUE', new Date(), {});
+
+      await expect(actualPromise).rejects.toThrow(RetryableError);
+      await expect(actualPromise).rejects.toThrow('🕰️');
+    });
+
+    it('should rethrow credential transient errors as retryable errors', async () => {
+      const internal = new Error('🔧');
+      (internal as any).code = status.INTERNAL;
+      getCredentialsSpy.mockRejectedValueOnce(internal);
+
+      const actualPromise2 = scheduler.schedule('MY_QUEUE', new Date(), {
+        oidcToken: 'self',
+      });
+
+      await expect(actualPromise2).rejects.toThrow(RetryableError);
+      await expect(actualPromise2).rejects.toThrow('🔧');
+    });
   });
 });
