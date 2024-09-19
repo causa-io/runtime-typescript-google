4d2870b1973e191fec0325ad7c4eb00b22b99c17
Flo <florent.vouin@gmail.com>
2024-09-19 10:17:10 +0200

    ✨ Make the FirestoreHealthIndicator get the database instead of listing collections

--- src/firestore/healthcheck.ts
+++ src/firestore/healthcheck.ts
@@ -2,6 +2,7 @@ import { BaseHealthIndicatorService } from '@causa/runtime/nestjs';
 import { Injectable } from '@nestjs/common';
 import { HealthCheckError, HealthIndicatorResult } from '@nestjs/terminus';
 import { Firestore } from 'firebase-admin/firestore';
+import { FirestoreAdminClient } from '../firebase/index.js';
 
 /**
  * The key used to identify the Firestore health indicator.
@@ -13,17 +14,30 @@ const FIRESTORE_HEALTH_KEY = 'google.firestore';
  */
 @Injectable()
 export class FirestoreHealthIndicator extends BaseHealthIndicatorService {
-  constructor(private readonly firestore: Firestore) {
+  /**
+   * The ID of the database used by Firestore.
+   */
+  private readonly databaseId: string;
+
+  constructor(
+    private readonly admin: FirestoreAdminClient,
+    firestore: Firestore,
+  ) {
     super();
+
+    this.databaseId = firestore.databaseId;
   }
 
   async check(): Promise<HealthIndicatorResult> {
     try {
-      await this.firestore.listCollections();
+      const projectId = await this.admin.getProjectId();
+      const name = this.admin.databasePath(projectId, this.databaseId);
+      await this.admin.getDatabase({ name });
+
       return this.getStatus(FIRESTORE_HEALTH_KEY, true);
     } catch (error: any) {
       throw new HealthCheckError(
-        'Failed to check health by listing Firestore collections.',
+        'Failed to check health by getting Firestore database.',
         this.getStatus(FIRESTORE_HEALTH_KEY, false, {
           error: error.message,
         }),
