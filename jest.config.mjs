83921e012a0d7d397cb94a8542f0755ae520d448
Flo <florent.vouin@gmail.com>
2024-11-14 16:25:00 +0100

    🔧 Workaround Jest configuration issue

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   clearMocks: true,
   coverageDirectory: '../coverage',
   collectCoverageFrom: [
