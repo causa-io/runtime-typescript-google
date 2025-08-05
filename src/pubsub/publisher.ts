09544fd7e648e914ec6d6fd5bf44626f6417d9aa
Flo <florent.vouin@gmail.com>
2025-08-05 15:36:33 +0200

    🔇 Do not log an error when failing to publish as the error is not caught

--- src/pubsub/publisher.ts
+++ src/pubsub/publisher.ts
@@ -195,32 +195,16 @@ export class PubSubPublisher implements EventPublisher, OnApplicationShutdown {
       messageInfo.eventId = attributes.eventId;
     }
 
-    try {
-      const messageId = await pubSubTopic.publishMessage({
-        data,
-        attributes,
-        orderingKey: key,
-      });
+    const messageId = await pubSubTopic.publishMessage({
+      data,
+      attributes,
+      orderingKey: key,
+    });
 
-      this.logger.info(
-        { publishedMessage: { ...messageInfo, messageId } },
-        'Published message to Pub/Sub.',
-      );
-    } catch (error: any) {
-      this.logger.error(
-        {
-          failedMessage: {
-            ...messageInfo,
-            data: data.toString('base64'),
-            attributes,
-            key,
-          },
-          error: error.stack,
-        },
-        'Failed to publish message to Pub/Sub.',
-      );
-      throw error;
-    }
+    this.logger.info(
+      { publishedMessage: { ...messageInfo, messageId } },
+      'Published message to Pub/Sub.',
+    );
   }
 
   async flush(): Promise<void> {
