f7baa3106e62a20c13e632c2c93eee23ecf30aa2
Flo <florent.vouin@gmail.com>
2026-01-12 10:43:27 +0100

    ♻️ Use the HealthIndicatorService in existing health indicators

--- src/firestore/healthcheck.ts
+++ src/firestore/healthcheck.ts
@@ -1,6 +1,13 @@
-import { BaseHealthIndicatorService } from '@causa/runtime/nestjs';
+import { orFallbackFn, tryMap } from '@causa/runtime';
+import {
+  BaseHealthIndicatorService,
+  type HealthChecker,
+} from '@causa/runtime/nestjs';
 import { Injectable } from '@nestjs/common';
-import { HealthCheckError, type HealthIndicatorResult } from '@nestjs/terminus';
+import {
+  HealthIndicatorService,
+  type HealthIndicatorResult,
+} from '@nestjs/terminus';
 import { Firestore } from 'firebase-admin/firestore';
 
 /**
@@ -12,22 +19,25 @@ const FIRESTORE_HEALTH_KEY = 'google.firestore';
  * A service testing the availability of the Firestore service.
  */
 @Injectable()
-export class FirestoreHealthIndicator extends BaseHealthIndicatorService {
-  constructor(private readonly firestore: Firestore) {
+export class FirestoreHealthIndicator
+  extends BaseHealthIndicatorService
+  implements HealthChecker
+{
+  constructor(
+    private readonly firestore: Firestore,
+    private readonly healthIndicatorService: HealthIndicatorService,
+  ) {
     super();
   }
 
   async check(): Promise<HealthIndicatorResult> {
-    try {
-      await this.firestore.listCollections();
-      return this.getStatus(FIRESTORE_HEALTH_KEY, true);
-    } catch (error: any) {
-      throw new HealthCheckError(
-        'Failed to check health by listing Firestore collections.',
-        this.getStatus(FIRESTORE_HEALTH_KEY, false, {
-          error: error.message,
-        }),
-      );
-    }
+    const check = this.healthIndicatorService.check(FIRESTORE_HEALTH_KEY);
+    return await tryMap<HealthIndicatorResult>(
+      async () => {
+        await this.firestore.listCollections();
+        return check.up();
+      },
+      orFallbackFn((error: any) => check.down({ error: error.message })),
+    );
   }
 }
--- src/pubsub/healthcheck.ts
+++ src/pubsub/healthcheck.ts
@@ -1,8 +1,15 @@
-import { BaseHealthIndicatorService } from '@causa/runtime/nestjs';
+import { orFallbackFn, tryMap } from '@causa/runtime';
+import {
+  BaseHealthIndicatorService,
+  type HealthChecker,
+} from '@causa/runtime/nestjs';
 import { PubSub } from '@google-cloud/pubsub';
 import { status } from '@grpc/grpc-js';
 import { Injectable } from '@nestjs/common';
-import { HealthCheckError, type HealthIndicatorResult } from '@nestjs/terminus';
+import {
+  HealthIndicatorService,
+  type HealthIndicatorResult,
+} from '@nestjs/terminus';
 
 /**
  * The key used to identify the Pub/Sub health indicator.
@@ -13,29 +20,32 @@ const PUBSUB_HEALTH_KEY = 'google.pubSub';
  * A service testing the availability of the Pub/Sub service.
  */
 @Injectable()
-export class PubSubHealthIndicator extends BaseHealthIndicatorService {
-  constructor(private readonly pubSub: PubSub) {
+export class PubSubHealthIndicator
+  extends BaseHealthIndicatorService
+  implements HealthChecker
+{
+  constructor(
+    private readonly pubSub: PubSub,
+    private readonly healthIndicatorService: HealthIndicatorService,
+  ) {
     super();
   }
 
   async check(): Promise<HealthIndicatorResult> {
-    try {
-      await this.pubSub.getTopics({
-        autoPaginate: false,
-        pageSize: 1,
-      });
-    } catch (error: any) {
-      // Permission denied errors are treated as healthy, as they indicate that the service could successfully connect
-      // to the Pub/Sub service (which is probably healthy), but was not allowed to list topics (which is usually not
-      // the case for publishers).
-      if (error.code !== status.PERMISSION_DENIED) {
-        throw new HealthCheckError(
-          'Failed to check health by retrieving Pub/Sub topics.',
-          this.getStatus(PUBSUB_HEALTH_KEY, false, { error: error.message }),
-        );
-      }
-    }
-
-    return this.getStatus(PUBSUB_HEALTH_KEY, true);
+    const check = this.healthIndicatorService.check(PUBSUB_HEALTH_KEY);
+    return await tryMap<HealthIndicatorResult>(
+      async () => {
+        await this.pubSub.getTopics({ autoPaginate: false, pageSize: 1 });
+        return check.up();
+      },
+      orFallbackFn((error: any) => {
+        // Permission denied errors are treated as healthy, as they indicate that the service could successfully connect
+        // to the Pub/Sub service (which is probably healthy), but was not allowed to list topics (which is usually not
+        // the case for publishers).
+        return error.code === status.PERMISSION_DENIED
+          ? check.up()
+          : check.down({ error: error.message });
+      }),
+    );
   }
 }
