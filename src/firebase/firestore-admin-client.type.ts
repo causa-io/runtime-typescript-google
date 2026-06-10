93ede188de258dee4c5734ebca2cbec65d4b5efb
Flo <florent.vouin@gmail.com>
2026-06-10 11:51:05 +0200

    🚨 Fix compiler error on FirestoreAdminClient type

--- src/firebase/firestore-admin-client.type.ts
+++ src/firebase/firestore-admin-client.type.ts
@@ -3,5 +3,6 @@ import { v1 } from 'firebase-admin/firestore';
 /**
  * The low-level client to access administrative Firestore operations.
  */
-export const FirestoreAdminClient = v1.FirestoreAdminClient;
+export const FirestoreAdminClient: typeof v1.FirestoreAdminClient =
+  v1.FirestoreAdminClient;
 export type FirestoreAdminClient = InstanceType<typeof v1.FirestoreAdminClient>;
